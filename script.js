816418e05e808c06304915b66ac2222e1d57eb24
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2022-02-02 00:30:11 +0530

    commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 weather={
     apiKey:"d72c2582c3b119e2fd59a8b6fac30dec",
     fetchWeather : function (city){
-        fetch("http://api.openweathermap.org/data/2.5/weather?q="
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
         +city
         +"&units=metric&appid="
         +this.apiKey

064abf0fb0fcb9f50b79a7d83543343d24e1d17a
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2022-02-01 23:12:02 +0530

    final commit

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 weather={
     apiKey:"d72c2582c3b119e2fd59a8b6fac30dec",
     fetchWeather : function (city){
-        fetch("api.openweathermap.org/data/2.5/weather?q="
+        fetch("http://api.openweathermap.org/data/2.5/weather?q="
         +city
         +"&units=metric&appid="
         +this.apiKey
         )
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data));
+        
+    },
+    displayWeather : function(data) {
+       const { name } = data
+       const { temp } = data.main
+       const { icon } = data.weather[0]
+       const { description } = data.weather[0]
+       const { humidity } = data.main
+       const { speed } = data.wind
+       document.querySelector(".city").innerHTML = "Weather in "+name;
+       document.querySelector(".temp").innerHTML = temp+" °C";
+       document.querySelector(".icon").src="http://openweathermap.org/img/wn/"+icon+".png";
+       document.querySelector(".description").innerHTML = description;
+       document.querySelector(".humidity").innerHTML = "Humidity : "+humidity+"%";
+       document.querySelector(".wind").innerHTML = "Wind speed : "+speed+"m/s";
+       document.querySelector(".weather").classList.remove("loading");
+       document.body.style.backgroundImage =
+       "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search : function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 }
 
+document.querySelector(".search button").addEventListener("click", function(){
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if(event.key === "Enter"){
+        weather.search();
+    }
+});
+
+// weather.fetchWeather("नई दिल्ली");
